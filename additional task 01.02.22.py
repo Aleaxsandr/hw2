512cdc374ddc338f06b0d40eab5632b3f003b03e
Анихимовский Александр <98551398+Aleaxsandr@users.noreply.github.com>
2022-02-01 14:33:42 +0300

    Add files via upload

---
+++ additional task 01.02.22.py
@@ -0,0 +1,15 @@
+# программа приветствия.
+name = input('Введите имя: ')
+print('Hello,' ,name+'!')  # знак + убирает пробел.
+
+# 20 переменных со значениями
+my_name = 'Alexsander'
+my_wife = 'Svetlana'
+my_daugher = 'Marina'
+my_son = 'Bladislav'
+my_mother = 'Taisiya'
+my_father = 'Balera'
+my_sister = 'Elena'
+my_car = 'Peouget'
+print(my_name,'+',my_wife, '=', my_daugher)
+print(my_father,'+',my_mother, '=', my_name, 'and', my_sister)
